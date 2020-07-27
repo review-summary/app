7e188606e29c71474db0c72357a315513acb33b7
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2020-07-27 18:12:03 +0200

    Add preprocess method

--- app/app.py
+++ app/app.py
@@ -4,19 +4,20 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+def preprocess(documents: Collection[dict]) -> Collection[dict]:
+    return documents
+
+
 def predict(documents: Collection[dict]) -> Collection[dict]:
-    """Mock of the predict method, to be replaced with the code that we will use for making predictions.
-    
-    Currently it is just an identity function, but later we want to put the inference function in here.
-    """
     return documents
 
 
 @app.route('/', methods = ['POST'])
 def main():
     data = request.get_json()
-    preds = predict(data)
-    return jsonify(preds)
+    data = preprocess(data)
+    pred = predict(data)
+    return jsonify(pred)
 
 
 if __name__=="__main__":
