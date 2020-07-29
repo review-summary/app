619e98f935c379e106324cbedca718726dee766b
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2020-07-29 20:58:45 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM python:3.7.8-slim-buster
-
 ENV PYTHONPATH /app:$PYTHONPATH
-
-COPY app/ /app
-
 WORKDIR /app
 
+COPY app/requirements.txt /app/
 RUN pip install -r /app/requirements.txt
+
+COPY app/ /app
 
 CMD ["python", "app.py"]
--- app/app.py
+++ app/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from models.arbitrary import predict
 
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods=['POST'])
 def main():
     data = request.get_json()
     return jsonify(predict(data))
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    products = [
+        "Disney Mickey Mouse Deluxe Boys' Costume",
+        "Levi's Men's 501 Original Fit Jeans, Polished Black, 44W x 30L",
+        "Levi's Men's 501 Original-Fit Jeans, Trashed, 33W x 32L",
+        "Hanes Absolutely Ultra Sheer Sheer Control Top SF (Single) Size:E Color:Jet",
+        "Merrell Women's Jungle Moc Taupe Slip-On Shoe - 8.5 B(M) US",
+        "sofsy Soft-Touch Rayon Blend Tie Front Nursing & Maternity Fashion Top Charcoal Small",
+        "The Last Life: A Novel",
+    ]
+    if request.method == 'POST':
+        selected = request.form.get("products", "")
+        return render_template('form.html', products=products, selected=selected)
+    return render_template('form.html', products=products)
 
 
 if __name__=="__main__":
