f3d54065e44e20474000aa11a1fd821d1f0855b4
Sheng Xu <sheng.xu@shopify.com>
2020-08-07 00:36:05 -0400

    refactor

--- app/app.py
+++ app/app.py
@@ -30,34 +30,47 @@ def form():
         # sorted_topic_review_df_5_t0, sorted_topic_review_df_5_t1 = review_2_topic(documents_rating_5, model_5, bow_corpus_5)
         # print(sorted_topic_review_df_5_t0)
         number_of_sentences_display = m
-        final_display = {}
-        data = load_data("app/sample_reviews.json", product_name)
-        corpus_embeddings = bert_model(product_name)
-        cluster_assignment, centers = clustering(corpus_embeddings, num_clusters = 5)
-        for k in range(num_clusters):
-            cluster_indices = np.where(cluster_assignment == k)[0]
-            cluster_embeddings = [corpus_embeddings[i] for i in cluster_indices]
-            distance_from_centroids = scipy.spatial.distance.cdist(centers, cluster_embeddings, "cosine")
-            closest_sentence_indices = distance_from_centroids[4].argsort()[-m:][::-1]
-            display_sentences = [corpus[j] for j in closest_sentence_indices]
-            final_display[k] = display_sentences
-        p1 = final_display[0][0]
-        p2 = final_display[0][1]
-        p3 = final_display[0][2]
-        n1 = final_display[0][1]
-        n2 = final_display[0][1]
-        n3 = final_display[0][1]
-        print(p1, p2, n1, n2)
+        num_clusters = 5
+        final_display_1 = {}
+        final_display_5 = {}
+        # data = load_data("app/sample_reviews.json", product_name)
+        #TODO add filter for star rating here
+        # print("Processing Reviews...")
+        # ratings_documents = read_split('app/sample_reviews.json', product_name)
+        # documents_rating_1 = ratings_documents[1]
+        # documents_rating_5 = ratings_documents[5]
+        corpus_embeddings_1, corpus_embeddings_5 = bert_model(product_name)
+        cluster_assignment_1, centers_1 = clustering(corpus_embeddings_1, num_clusters = 5)
+        cluster_assignment_5, centers_5 = clustering(corpus_embeddings_5, num_clusters = 5)
+
+        def display(num_clusters, cluster_assignment, corpus_embeddings, cluster_centers, corpus):
+            for k in range(num_clusters):
+                cluster_indices = np.where(cluster_assignment == k)[0]
+                cluster_embeddings = [corpus_embeddings[i] for i in cluster_indices]
+                distance_from_centroids = scipy.spatial.distance.cdist(cluster_centers, cluster_embeddings, "cosine")
+                closest_sentence_indices = distance_from_centroids[4].argsort()[-m:][::-1]
+                display_sentences = [corpus[j] for j in closest_sentence_indices]
+                final_display[k] = display_sentences
+            return final_display
+        final_display_1 = display(num_clusters, cluster_assignment_1, corpus_embedding_1, centers_1, corpus)
+        final_display_5 = display(num_clusters, cluster_assignment_5, corpus_embedding_5, centers_5, corpus)
+        p1 = final_display_5[0][0]
+        p2 = final_display_5[0][1]
+        p3 = final_display_5[0][2]
+        n1 = final_display_1[0][1]
+        n2 = final_display_1[0][1]
+        n3 = final_display_1[0][1]
+        print(p1, p2, p3, n1, n2, n3)
     else:
         selected = ""
-        p1 = p2 = n1 = n2 = ""
+        p1 = p2 = p3 = n1 = n2 = n3 = ""
     return render_template('form.html', products=products, selected=selected, 
     pos1=p1, 
     pos2=p2, 
-    # pos3=p3, 
+    pos3=p3, 
     neg1=n1,
-    neg2=n2)
-    # neg3=n3)
+    neg2=n2,
+    neg3=n3)
 
 
 if __name__=="__main__":
--- app/models/bert.py
+++ app/models/bert.py
@@ -3,7 +3,12 @@ from sentence_transformers import SentenceTransformer
 def bert_model(product_name):
     model = SentenceTransformer('bert-base-nli-mean-tokens')
 
-    data = load_data("app/sample_reviews.json", product_name)
-    corpus = data['reviewText'].to_list()
-    corpus_embeddings = embedder.encode(corpus)
-    return corpus_embeddings
+    print("Processing Reviews...")
+    ratings_documents = read_split('app/sample_reviews.json', product_name)
+    documents_rating_1 = ratings_documents[1]
+    documents_rating_5 = ratings_documents[5]
+    corpus_1 = documents_rating_1['reviewText'].to_list()
+    corpus_5 = documents_rating_5['reviewText'].to_list()
+    corpus_embeddings_1 = embedder.encode(corpus_1)
+    corpus_embeddings_5 = embedder.encode(corpus_5)
+    return corpus_embeddings_1, corpus_embeddings_5
